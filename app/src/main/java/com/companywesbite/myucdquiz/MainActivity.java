f738e9de02aaa240e92fc8229d66ff7a7893fc78
mmnete <mmnete@trinity.edu>
2018-10-28 16:22:25 +0000

    Add a Splashscreen. I also added a sample list view of how the quizes the user saved will be viewed.

--- app/src/main/java/com/companywesbite/myucdquiz/MainActivity.java
+++ app/src/main/java/com/companywesbite/myucdquiz/MainActivity.java
@@ -1,13 +1,62 @@
 package com.companywesbite.myucdquiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.companywesbite.myucdquiz.utils.QuizListViewAdapter;
+
+
+/*********
+ * This is the class that displays the items with the numbers
+ * Those will be transformed to the different quizes the user has saved
+ * We can simply query information from the database and display them in this listview
+ * Each item has a button so that the user can press to view the quiz and do all other stuff
+ * There is another button to take a quiz.
+ * 
+ */
+
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    Context context;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context=this;
+        ListView lstview=(ListView)findViewById(R.id.quizList);
+        lstview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Toast.makeText(context, "An item of the ListView is clicked.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        String[] items={"1","2","3","4","5","6","7","8","9","10","11","12"};
+        QuizListViewAdapter adapter=new QuizListViewAdapter(this,R.layout.quiz_item,R.id.quizName,items);
+        // Bind data to the ListView
+        lstview.setAdapter(adapter);
+
+
+
+
     }
+
+    public void clickMe(View view){
+        Button bt=(Button)view;
+        Toast.makeText(this, "Button "+bt.getText().toString(),Toast.LENGTH_LONG).show();
+    }
+
+
+
 }
---
+++ app/src/main/java/com/companywesbite/myucdquiz/SplashScreenActivity.java
@@ -0,0 +1,21 @@
+package com.companywesbite.myucdquiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Android Group:
+ */
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
