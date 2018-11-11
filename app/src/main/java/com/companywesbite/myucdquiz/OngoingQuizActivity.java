14bc0dfb6f033328bbab5e898c5b5685a371608f
Arya-L <892973943@qq.com>
2018-11-11 16:55:41 +0000

    notification when the activity is interrupted or the user clicks home button

--- app/src/main/java/com/companywesbite/myucdquiz/OngoingQuizActivity.java
+++ app/src/main/java/com/companywesbite/myucdquiz/OngoingQuizActivity.java
@@ -1,5 +1,10 @@
 package com.companywesbite.myucdquiz;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,6 +12,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -81,12 +87,38 @@ public class OngoingQuizActivity extends AppCompatActivity{
             }
         });
 
-
-
-
+        // This is where the quiz is going on
+        // we need a notification when the activity is interrupted or the user clicks home button
+        // when the notification is clicked, this activity does onResume()
+        // In the notification the title can be .. ongoing quiz
 
     }
 
+	@Override
+    protected void onPause() {
+        super.onPause();
+		// Set the startup program, if it exists, find out, otherwise new startup
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.addCategory(Intent.CATEGORY_LAUNCHER);
+		intent.setComponent(new ComponentName(this, OngoingQuizActivity.class));//Get the class object with ComponentName
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);// The key step is to set the startup mode, two cases
+		
+		NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+			.setContentTitle("QUIZ")
+            .setContentText("ongoing quiz.");
+        Notification notification = builder.build();
+	    
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, 0);//Bind the set Intent to the PendingIntent
+		notification.contentIntent = contentIntent;// Notification binding PendingIntent
+		notification.flags=Notification.FLAG_AUTO_CANCEL;//Set auto cancel
+		NotificationManager manager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+		manager.notify(1, notification);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
 
     // when the user clicks the back button .. we cannot let the user come back here
